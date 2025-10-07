f429d5c8048a3ed98bc29c515048bd904a08b8bc
LeoParlaforce <TON_EMAIL@exemple.com>
2025-10-07 09:33:40 +0200

    add stripe webhook route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.resolve(__dirname), // force le bon root
+  trailingSlash: false,
+  async redirects() {
+    return [
+      { source: "/api/:path*", destination: "/api/:path*", permanent: false, basePath: false },
+      { source: "/api/webhooks/stripe", destination: "/api/webhooks/stripe", permanent: false, basePath: false },
+    ];
+  },
 };
 
 export default nextConfig;

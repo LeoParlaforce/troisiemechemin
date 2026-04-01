c489aa4cb413b86a762b3eac226cde687c878fdc
LeoParlaforce <TON_EMAIL@exemple.com>
2026-04-01 11:04:14 +0200

    oiehnf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-  // pas de redirects, pas de rewrites ici
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.troisiemechemin.fr' }],
+        destination: 'https://troisiemechemin.fr/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
